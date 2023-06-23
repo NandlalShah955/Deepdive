b01591b428adebcefc67cd407e0c0233018a7a7f
Nandlal <shahnandlal49@gmail.com>
2023-06-23 10:28:46 +0530

    added login page

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import "../Components/Style.css"
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+<div className='hero'>
+    <div className='form-box'>
+        <div className='button-box'>
+        <div className='btn'></div>
+            <button type='button' className='toggle-btn'>Log In</button>
+            <button type='button' className='toggle-btn'>Register</button>
+        </div>
+   <form className='input-group'>
+    <input type="text" className='input-field' placeholder='User Id' />
+    <input type="text" className='input-field' placeholder='Enter Password' />
+    <button type='submit' className='submit-btn'>Submit</button>
+   </form>
+   
+    </div>
+</div>
+
+
+    </>
   )
 }
 
