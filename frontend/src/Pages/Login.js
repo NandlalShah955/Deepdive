d5a57ce3c5c54ae0344190ba75e296c32d6b5430
Nandlal <shahnandlal49@gmail.com>
2023-06-23 12:19:37 +0530

    added login page

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,28 +1,162 @@
-import React from 'react'
-import "../Components/Style.css"
+import React, { useState } from "react";
+import "../Components/Style.css";
+import {Link,useNavigate} from "react-router-dom";
 
 function Login() {
-  return (
-    <>
-<div className='hero'>
-    <div className='form-box'>
-        <div className='button-box'>
-        <div className='btn'></div>
-            <button type='button' className='toggle-btn'>Log In</button>
-            <button type='button' className='toggle-btn'>Register</button>
-        </div>
-   <form className='input-group'>
-    <input type="text" className='input-field' placeholder='User Id' />
-    <input type="text" className='input-field' placeholder='Enter Password' />
-    <button type='submit' className='submit-btn'>Submit</button>
-   </form>
-   
-    </div>
-</div>
+//   For implementing that sliding thing in login and signup
+  
+    const [loginLeft, setLoginLeft] = useState("50px");
+  const [registerLeft, setRegisterLeft] = useState("-400px");
+  const [btnLeft, setBtnLeft] = useState("0");
+  const [isButton1Clicked, setButton1Clicked] = useState(false);
+  const [isButton2Clicked, setButton2Clicked] = useState(false);
+
+//   For registering user 
+
+const [registerdetails, setregisterdetails] = useState({});
+const [loading, setloading] = useState(false);
+const navigate=useNavigate();
 
 
-    </>
-  )
+
+
+// For getting input from the user 
+const handlechange=()=>{
+    
+}
+// For Register the button 
+const registerbutton=()=>{
+
 }
 
-export default Login
+
+
+// For Login the user 
+const loginbutton=()=>{
+
+}
+
+
+
+
+// For handling sliding between login and register 
+const handlelogin = () => {
+    setLoginLeft("50px");
+    setRegisterLeft("450px");
+    setBtnLeft("0");
+    setButton1Clicked(true);
+    setButton2Clicked(false);
+};
+// For handling sliding between login and register 
+  const handleregister = () => {
+    setLoginLeft("-400px");
+    setRegisterLeft("50px");
+    setBtnLeft("0");
+    setButton1Clicked(false);
+    setButton2Clicked(true);
+  };
+
+  return (
+    <>
+      <div className="hero">
+        <div className="form-box">
+          <div className="button-box">
+            <div className="btn"></div>
+            <button
+              type="button"
+              className="toggle-btn"
+              onClick={handlelogin}
+              style={{
+                backgroundColor: isButton1Clicked
+                  ? "rgb(152, 152, 199)"
+                  : "white",
+                borderRadius: "30px",
+              }}
+            >
+              Log In
+            </button>
+            <button
+              type="button"
+              className="toggle-btn"
+              onClick={handleregister}
+              style={{
+                left: btnLeft,
+                backgroundColor: isButton2Clicked
+                  ? "rgb(152, 152, 199)"
+                  : "white",
+                borderRadius: "30px",
+              }}
+            >
+              Register
+            </button>
+          </div>
+          {/* Form for the login  */}
+
+          <form id="login" className="input-group" style={{ left: loginLeft }}>
+            <span className="sapan">Email</span>
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Enter Your Email"
+           
+              
+            />
+            <span className="sapan">Password</span>
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Enter Password"
+           
+            />
+            <button type="submit" className="submit-btn" id="logind">
+              Login
+            </button>
+          </form>
+
+          {/* Form for registration */}
+
+          <form
+            id="register"
+            className="input-group"
+            style={{ left: registerLeft }}
+          >
+            <span className="sapan">User name</span>
+
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Enter Your Name"
+              onChange={handlechange}
+              name="username"
+            />
+            <span className="sapan">Email</span>
+
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Enter Your Email"
+              onChange={handlechange}
+              name="email"
+            />
+            <span className="sapan">Password</span>
+
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Enter Your Password"
+              onChange={handlechange}
+              name="password"
+            />
+            <input type="checkbox" className="checg-box" />
+            <span>I agree to the terms & conditons</span>
+            <button type="submit" className="submit-btn">
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
