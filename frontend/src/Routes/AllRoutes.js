f21002ad62817a6ae3fcc97bef3b3003e19c620f
Nandlal <shahnandlal49@gmail.com>
2023-06-23 09:49:17 +0530

    added login page

--- frontend/src/Routes/AllRoutes.js
+++ frontend/src/Routes/AllRoutes.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import Dashboard from '../Pages/Dashboard';
-import Homepage from '../Pages/Homepage';
-import {Route,Routes} from "react-router-dom";
+import React from "react";
+import Dashboard from "../Pages/Dashboard";
+import Homepage from "../Pages/Homepage";
+import Login from "../Pages/Login";
+import { Route, Routes } from "react-router-dom";
 function AllRoutes() {
   return (
     <Routes>
-<Route path='/' element={<Dashboard/>}/>
-<Route path='/homepage' element={<Homepage/>}/>
-
-
-
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/homepage" element={<Homepage />} />
     </Routes>
-  )
+  );
 }
 
-export default AllRoutes
+export default AllRoutes;
